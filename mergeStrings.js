4c94c01694bb6543b26072284445ae75083492e1
sirphemmiey <oluwafemiakinde@gmail.com>
2018-09-20 08:27:08 -0800

    Merge two strings alternatively

---
+++ mergeStrings.js
@@ -0,0 +1,13 @@
+function find(a, b) {
+    var i,
+        l = Math.min(a.length, b.length),
+        temp = '';
+
+    for( i = 0; i < l; i++) {
+        temp += a[i] + b[i];
+    }
+    return temp + a.slice(i) + b.slice(i);
+}
+var a = ['a', 'b', 'c'];
+var b = [1, 2, 3,];
+console.log(find(a, b));
